3d1f29002b75f36861b4c952c565a4dea8a31e89
Your Name <marcosvanillle@gmail.com>
2020-05-18 10:25:07 -0300

    [UPDATE]INICIANDO ESTUDOS COM JQUERY

---
+++ 01-jquery-essentials/scripts.js
@@ -0,0 +1,8 @@
+//Forma padrao de inicializar o jquery
+// $(document).ready(function () {
+// $(".jquery_essentials").html("jquery iniciado")
+// });
+//Forma padrao de inicializar o jquery Simplificada
+$(function () {
+$(".jquery_essentials").html("jquery Iniciado com Gatilho")
+});
