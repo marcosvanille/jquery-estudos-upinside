e409348d6f4d095028a172196bd26b1c5e81f000
Your Name <marcosvanillle@gmail.com>
2020-05-21 00:44:51 -0300

    [UPDATE]funcionalidade de carrgar outras lib

--- 01-jquery-essentials/scripts.js
+++ 01-jquery-essentials/scripts.js
@@ -2,12 +2,23 @@
 // $(document).ready(function () {
 // $(".jquery_essentials").html("jquery iniciado")
 // });
+
 //Forma padrao de inicializar o jquery Simplificada
 // $(function () {
 // $(".jquery_essentials").html("jquery Iniciado com Gatilho")
 // });
 
 //carregando antes de outra LIB
-jQuery(function($){
-$(".jquery_essentials").html("jquery antes de outra lib!");
+// jQuery(function($){
+// $(".jquery_essentials").html("jquery antes de outra lib!");
+// });
+
+//carregando depois de outra lib
+// var $j = jQuery.noConflict();
+// $j(document).ready(function(){
+// $j  (".jquery_essentials").html("jquery depois de outra lib!")
+// });
+
+$(function(){
+$(".jquery_essentials").html("jquery essentials")
 });
