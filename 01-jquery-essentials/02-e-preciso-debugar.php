385c19c20e1064617cd68f8bb91fe868ee6fb8ce
Your Name <marcosvanillle@gmail.com>
2020-05-21 00:57:38 -0300

    [UPDATE]adicionando novos arquivos

---
+++ 01-jquery-essentials/02-e-preciso-debugar.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Estudando jquery</title>
+</head>
+<body>
+<div class="jquery_essentials">#BoraProgramar!</div>
+
+<script src="../js/node_modules/jquery/dist/jquery.js"></script>
+<script src="scripts.js"></script>
+
+
+</body>
+</html>
--- 01-jquery-essentials/scripts.js
+++ 01-jquery-essentials/scripts.js
@@ -1,23 +1,3 @@
-//Forma padrao de inicializar o jquery
-// $(document).ready(function () {
-// $(".jquery_essentials").html("jquery iniciado")
-// });
-
-//Forma padrao de inicializar o jquery Simplificada
-// $(function () {
-// $(".jquery_essentials").html("jquery Iniciado com Gatilho")
-// });
-
-//carregando antes de outra LIB
-// jQuery(function($){
-// $(".jquery_essentials").html("jquery antes de outra lib!");
-// });
-
-//carregando depois de outra lib
-// var $j = jQuery.noConflict();
-// $j(document).ready(function(){
-// $j  (".jquery_essentials").html("jquery depois de outra lib!")
-// });
 
 $(function(){
 $(".jquery_essentials").html("jquery essentials")
